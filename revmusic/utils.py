96361bfa8a9f844f9290282bc6493d06c7849144
Zabrakk <ak4.jarvenpaa@gmail.com>
2021-02-20 17:03:37 +0200

    Return None if date is none

--- revmusic/utils.py
+++ revmusic/utils.py
@@ -5,9 +5,10 @@ def to_date(date_str):
     Convert a "%dd-%mm-%YYYY" string to a Date
     params:
     - date_str: The date string
-    Returns: Date or None if failed to convert
+    Returns: Date or None if failed to convert. None is also returned if date_str is None
     """
-
+    if date_str is None:
+        return None
     try:
         d, m, y = date_str.split('-')
         return datetime.date(int(y), int(m), int(d))
