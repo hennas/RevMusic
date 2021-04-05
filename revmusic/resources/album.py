9b40dc317345ee475201b593911fef8fe218bada
hennas <32382243+hennas@users.noreply.github.com>
2021-04-05 14:19:56 +0300

    PUT and DELETE for album item

--- revmusic/resources/album.py
+++ revmusic/resources/album.py
@@ -63,13 +63,13 @@ class AlbumCollection(Resource):
             release = to_date(request.json['release'])
             if release is None: # None returned in case of an error
                 return create_error_response(400, 'Invalid release date',
-                'The release data you provided is an invalid date')
+                'The release date you provided is an invalid date')
         duration = None
         if 'duration' in request.json:
             duration = to_time(request.json['duration'])
             if duration is None:# None returned in case of an error
                 return create_error_response(400, 'Invalid duration',
-                'The release duration you provided is an invalid time')
+                'The album duration you provided is an invalid time')
         genre = None
         if 'genre' in request.json:
             genre = request.json['genre']
@@ -92,6 +92,7 @@ class AlbumCollection(Resource):
             db.session.add(album)
             db.session.commit()
         except IntegrityError:
+            db.session.rollback()
             return create_error_response(409, 'Already exists',
             'Album with title "{}" already exists with artist "{}"'.format(title, artist))
 
@@ -103,7 +104,7 @@ class AlbumItem(Resource):
     def get(self, album):
         album_item = Album.query.filter_by(unique_name=album).first()
         if not album_item:
-            return create_error_response(404, "Album not found")
+            return create_error_response(404, 'Album not found')
         
         # Handle optional data (date and time objects to string)
         release = album_item.publication_date
@@ -132,7 +133,66 @@ class AlbumItem(Resource):
         return Response(json.dumps(body), 200, mimetype=MASON)
 
     def put(self, album):
+        if not request.json:
+            return create_error_response(415, 'Unsupported media type', 'Use JSON')
+        
+        try:
+            validate(request.json, Album.get_schema())
+        except ValidationError as e:
+            return create_error_response(400, 'Invalid JSON document', str(e))
+        
+        album_item = Album.query.filter_by(unique_name=album).first()
+        if not album_item:
+            return create_error_response(404, 'Album not found')
+        
+        unique_name = request.json['unique_name'].lower()
+        title = request.json['title']
+        artist = request.json['artist']
+        
+        # Handle optional arguments; will be set to null if not provided
+        release = None
+        if 'release' in request.json:
+            release = to_date(request.json['release'])
+            if release is None: # None returned in case of an error
+                return create_error_response(400, 'Invalid release date',
+                'The release date you provided is an invalid date')
+        duration = None
+        if 'duration' in request.json:
+            duration = to_time(request.json['duration'])
+            if duration is None: # None returned in case of an error
+                return create_error_response(400, 'Invalid duration',
+                'The album duration you provided is an invalid time')
+        genre = None
+        if 'genre' in request.json:
+            genre = request.json['genre']
+
+        # Check that unique_name is not in use in case that it is changed
+        if unique_name != album and Album.query.filter_by(unique_name=unique_name).count() > 0:
+            return create_error_response(409, 'Already exists',
+            'Unique name "{}" is already in use'.format(unique_name))
+
+        # Update album values and commit
+        album_item.unique_name = unique_name,
+        album_item.title = title,
+        album_item.artist = artist,
+        album_item.publication_date = release,
+        album_item.duration = duration,
+        album_item.genre = genre
+
+        try:
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+            return create_error_response(409, 'Already exists',
+            'Album with title "{}" already exists with artist "{}"'.format(title, artist))
+
         return Response(status=204)
 
     def delete(self, album):
+        album_item = Album.query.filter_by(unique_name=album).first()
+        if not album_item:
+            return create_error_response(404, 'Album not found')
+        
+        db.session.delete(album_item)
+        db.session.commit()
         return Response(status=204)
